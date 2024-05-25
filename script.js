ed88c774e01755f0231f9d0e3aac47ee1a45d244
shashank77t <110228304+shashank77t@users.noreply.github.com>
2024-05-25 14:19:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+
+const element =document.querySelector(".level");
+var level=0;
+  while(element!=null){
+	element=element.parentElement;
+	level++;
+  }
+
+alert("The level of the element is:" ${level})
